599773d69cfe405fd97b219a947271a1ff1281b8
Felippe Roberto Bayestorff Duarte <felippeduarte@gmail.com>
2018-12-03 09:56:57 -0200

    fixing commit assignment03

--- React/Assignment03/src/redux/configureStore.js
+++ React/Assignment03/src/redux/configureStore.js
@@ -1,17 +1,10 @@
-import {createStore, combineReducers} from 'redux';
-import { Dishes } from './dishes';
-import { Comments } from './comments';
-import { Promotions } from './promotions';
-import { Leaders } from './leaders';
+import {createStore} from 'redux';
+import { Reducer, initialState } from './reducer'
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
-            dishes: Dishes,
-            comments: Comments,
-            promotions: Promotions,
-            leaders: Leaders
-        })
+        Reducer, // reducer
+        initialState, // our initialState
     );
 
     return store;
